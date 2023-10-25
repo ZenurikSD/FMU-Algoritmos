6138643a522ea54b6bba6f9de7e1b32850c2d9c7
ZenurikSD <alissonsilva498@outlook.com>
2023-10-25 20:55:20 -0300

    First

---
+++ main.c
@@ -0,0 +1,5 @@
+/* TO-DO:
+ * 1. Implementar algoritmos de ordenação
+ * 2. Implementar código para gerar vetores e salvar eles em um arquivo para comparar o 
+ *    desempenho dos algoritmos com o mesmo vetor
+ */
