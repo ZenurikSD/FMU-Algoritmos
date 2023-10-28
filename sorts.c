1677cd05df8ed443ef26f3c18c270cd45f2a40c8
ZenurikSD <alissonsilva498@outlook.com.br>
2023-10-27 22:12:03 -0300

    Refatoração: Funções de sort desacopladas do main

---
+++ sorts.c
@@ -0,0 +1,57 @@
+/* Insira a implementação da função de sort aqui
+ * Copie a declaração da função no arquivo header para que funcione no main
+ */
+
+// Implementado por:
+int bubbleSort( )
+{
+
+}
+
+// Implementado por:
+void selectionSort( )
+{
+
+}
+
+// Implementado por:
+void insertionSort( )
+{
+
+}
+
+//Implementado por: Alisson
+void heapSort(int size, int *array)
+{
+
+}
+
+// Implementado por: Alisson
+void mergeSort(int *vet, int start, int end)
+{
+
+}
+
+//Implementado por: Alisson
+int quickSort(int *array, int low, int high)
+{
+
+}
+
+//Implementado por:
+void countSort( )
+{
+
+}
+
+//Implementado por:
+void bucketSort( )
+{
+
+}
+
+//Implementado por:
+void radixSort( )
+{
+
+}
---
+++ sorts.h
@@ -0,0 +1,25 @@
+#ifndef sorts_h
+#define sorts_h
+
+//Protótipos das funções de sort
+
+int bubbleSort( );
+
+void selectionSort( );
+
+void insertionSort( );
+
+void heapSort(int size, int *array);
+
+void mergeSort(int *vet, int start, int end);
+
+int quickSort(int *array, int low, int high);
+
+void countSort( );
+
+void bucketSort( );
+
+void radixSort( );
+
+
+#endif
