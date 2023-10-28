cc8649001711dc1aa7500564fa0f1992f9fd9cf7
ZenurikSD <alissonsilva498@outlook.com.br>
2023-10-28 00:25:57 -0300

    Mecanismo de gerar vários vetores

--- main.c
+++ main.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "sorts.h"
 
 void fillWithRandom(int *array, int size);
-void saveArray(int *array, int size, int fileName);
+void saveArray(int *array, int size, char *filename);
+int readArray(int *target, int size, char *filename);
 
 
-int main(int argc, char *argv)
+int main(void)
 {
     int *array;
     int size;
-    char fileName[200];
+    int array_qty = 5;
+    char filename[200];
 
     printf("Digite o tamanho do vetor: ");
     scanf("%i", &size);
 
     printf("Digite o nome do arquivo: ");
-    scanf("%s", &fileName);
+    scanf("%s", &filename);
 
+    // Preenche "n" vetores com valores aleatórios, fazendo umas coisas extras pra ter nomes de arquivos diferentes
     array = malloc(sizeof(int) * size);
-    fillWithRandom(array, size);
-    saveArray(array, size, fileName);
+    char new_name[strlen(filename) + 4];
+    char append[3];
+    for (int i = 0; i < array_qty; i++ ){
+        fillWithRandom(array, size);
+
+        sprintf(append, "-%i", i+1);
+        strcpy(new_name, filename);
+        strcat(new_name, append);
+
+        saveArray(array, size, new_name);
+    }
+
+    
 
 
 
     free(array);
+   
     return 0;
 }
 
@@ -43,9 +59,9 @@ void fillWithRandom(int *array, int size)
 
 
 /** Salva um vetor para um arquivo */
-void saveArray(int *array, int size, int fileName)
+void saveArray(int *array, int size, char *filename)
 {
-    FILE *write_p = fopen(fileName, "w");
+    FILE *write_p = fopen(filename, "w");
     if (write_p != NULL){
         fwrite(array, sizeof(int), size, write_p);
         fclose(write_p);
@@ -56,9 +72,9 @@ void saveArray(int *array, int size, int fileName)
  * @param target O vetor de destino (deve ter o mesmo tamanho do vetor no arquivo)
  * @param size O tamanho do vetor de destino
 */
-void readArray(int *target, int size, int fileName)
+int readArray(int *target, int size, char *filename)
 {
-    FILE *read_p = fopen(fileName, "r");
+    FILE *read_p = fopen(filename, "r");
     if (read_p != NULL){
         fread(target, sizeof(int), size, read_p);
         fclose(read_p);
